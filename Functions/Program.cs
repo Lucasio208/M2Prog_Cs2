3f951014be17cfb37995e701684e8cd0a56b982d
lucas zijlstra <40685@ma-web.nl>
2025-11-18 12:50:16 +0100

    update1

---
+++ Functions/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Functions;
+
+
+internal class Program //de function heet program //en het is de code die we eronder hebben getypt
+{                      //de return type is void omdat we niks teruggeven
+   
+   static void Main(string[] args)
+   {
+        Program program = new Program(); // we hebben variable program gemaakt //program is een program      
+        program.Run();      
+   }
+ 
+    internal void Run()
+    {
+
+        Console.WriteLine("Dit is nu de start van mij programma");
+
+    }
+
+        
+
+    
+
+}
+
+
+
+
+

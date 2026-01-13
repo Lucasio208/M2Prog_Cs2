5a81f7e18d2514ff594f5a6631664facc0b76aa6
lucas zijlstra <40685@ma-web.nl>
2026-01-13 12:35:10 +0100

    opdr1

---
+++ ForHerhaling/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ForHerhaling;
+
+class Program{
+
+static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.Run;
+    }
+
+    internal void Run()
+    {
+        double[] prijzen = new double[] {0.99, 5.60, 10.11};
+        string[] artiekelen = {"snoepje", "luxe broodje", "Lunch menu"};    
+
+        formulieren[0] = newFormulier();{
+        Feedback = "prijzig maar lekker";
+        Sterren = 5;
+        };
+
+        formulieren[0] = newFormulier();{
+        Feedback = "prijzig maar lekker";
+        Sterren = 5;
+        }
+    }
+
+    internal class Formulier
+    {
+        public string Sterren { get; set; }
+        public int Feedback { get; set; }
+    }
+
+}
---
+++ ForHerhaling/filelo/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace filelo;
+
+class Program
+{
+
+    
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.run();
+        }
+    
+    
+    private void run()
+    {
+        Console.WriteLine("Hello World!");
+    }
+
+
+}
